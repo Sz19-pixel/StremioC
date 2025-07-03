32cec98fd44ed202193bba5f72d5946080f074f8
Sz19-pixel <trytry11101@gmail.com>
2025-07-03 21:08:51 +0300

    Create build.gradle.kts

---
+++ StremioC/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    language = "en"
+    description = "Stremio Catalog Plugin"
+    authors = listOf("YourName")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+
+    iconUrl = "https://raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/main/StremioX/icon.png"
+}
